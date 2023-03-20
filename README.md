c7f3edef1d3daf478fdd550df208ed1d79cee9c2
migvas <migas.v94@gmail.com>
2023-03-20 23:35:12 +0000

    feat: Add rock paper scissors game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+Simple rock paper scissors game made in javascript that can be played in the browser.
---
+++ scripts.js
@@ -0,0 +1,81 @@
+const options = ['rock', 'paper', 'scissors'];
+
+const getComputerChoice = () => {
+    const choice = Math.floor(Math.random() * 3);
+    return options[choice]
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection === 'paper') {
+                console.log("You Lose! Paper beats Rock");
+                return -1;
+            } else if (computerSelection === 'scissors') {
+                console.log("You Win! Rock beats Scissors");
+                return 1;
+            } else {
+                console.log("It's a Tie! Both picked Rock");
+                return 0;
+            }
+
+        case 'paper':
+            if (computerSelection === 'scissors') {
+                console.log("You Lose! Scissors beats Paper");
+                return -1;
+            } else if (computerSelection === 'rock') {
+                console.log("You Win! Paper beats Rock");
+                return 1;
+            } else {
+                console.log("It's a Tie! Both picked Paper");
+                return 0;
+            }
+
+        case 'scissors':
+            if (computerSelection === 'rock') {
+                console.log("You Lose! Rock beats Scissors");
+                return -1;
+            } else if (computerSelection === 'paper') {
+                console.log("You Win! Scissors beats Paper");
+                return 1;
+            } else {
+                console.log("It's a Tie! Both picked Scissors");
+                return 0;
+            }
+
+        default:
+            console.log("Invalid pick");
+            throw ('Invalid')
+    }
+}
+
+
+const game = () => {
+    let counter = 0;
+
+    let g = 0;
+
+    while (g < 5) {
+        const playerSelection = prompt('Rock, Paper, Scissors');
+        const computerSelection = getComputerChoice();
+
+        try {
+            counter += playRound(playerSelection, computerSelection);
+            g++;
+        } catch (e) {
+            console.log('Invalid Round')
+        }
+    }
+
+    if (counter > 0) {
+        console.log('Player wins!');
+    } else if (counter < 0) {
+        console.log('Player looses!');
+    } else {
+        console.log('Tie!');
+    }
+}
+
+game()
